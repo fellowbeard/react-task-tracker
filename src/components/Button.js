4b9fcd6b111db7a3d953842238ed6450bd91367c
fellowbeard <virgovibe@gmail.com>
2022-12-27 17:10:05 -0600

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export const Button = ({ color, text }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+  return (
+    <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
